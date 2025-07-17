3b2e100c9f0a234f9c088792828bec119cb21958
DESHIKATHIKSHANI <deshikathikshani456@gmail.com>
2025-07-18 04:02:17 +0530

    fix: simplify no items message rendering in ListGroup component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,17 +4,12 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
 
-    // if (items.length === 0) {
-    //     return <>
-    //         <h1>My List Group</h1>
-    //         <p>No items found.</p>
-    //     </>
-    // }
+  const message = items.length === 0 ? "No items found." : null;
 
   return (
     <>
       <h1>My List Group</h1>
-      {items.length === 0 ? <p>No items found.</p> : null}
+      {message}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
