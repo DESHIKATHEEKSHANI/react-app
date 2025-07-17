bdc0bba1c054c87bbe27809fd867fc7c7237d43d
DESHIKATHIKSHANI <deshikathikshani456@gmail.com>
2025-07-18 02:42:47 +0530

    feat: replace default app content with Message component

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+
+//PascalCasing
+function Message() {
+    //JSX: JavaScript XML
+    //JSX is a syntax extension for JavaScript that looks similar to XML or HTML.
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
+//Exporting the Message component as the default export of this module
