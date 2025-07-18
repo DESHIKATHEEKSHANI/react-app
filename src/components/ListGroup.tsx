13faecedef54ec4ae4f5f8b5fb6ccbe23ba4a1e7
DESHIKATHIKSHANI <deshikathikshani456@gmail.com>
2025-07-19 04:28:58 +0530

    fix: update ListGroup component to accept onSelectItem prop and handle item selection

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,10 +4,11 @@ import { useState } from "react";
 interface ListGroupProps {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
 
-function ListGroup({ items, heading }: ListGroupProps) {
+function ListGroup({ items, heading, onSelectItem}: ListGroupProps) {
   
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
@@ -26,6 +27,7 @@ function ListGroup({ items, heading }: ListGroupProps) {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
