d5e642a68df111d939a83da544f93aeb52e92a20
DESHIKATHIKSHANI <deshikathikshani456@gmail.com>
2025-07-19 03:46:29 +0530

    fix: refactor ListGroup component to use a dedicated click handler for item clicks

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,22 @@
 import { Fragment } from "react";
+import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  // Event handler for item click
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>My List Group</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item, index) => (
+        {items.map((item) => (
           <li
             className="list-group-item"
             key={item}
-            onClick={() => console.log(item, index)}
+            onClick={handleClick}
           >
             {item}
           </li>
