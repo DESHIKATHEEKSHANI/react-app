01454aef2b14d35ec93cfda7a602414d0e11b9f4
DESHIKATHIKSHANI <deshikathikshani456@gmail.com>
2025-07-19 04:49:02 +0530

    feat: implement Alert component to display alert messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import ListGroup from "./components/ListGroup";  
+import Alert from "./components/Alert";
 
 function App() {
-    let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-    const handleSelectItem = (item: string) => {
-        console.log("Selected item:", item);
-    };
     return (
         <div>
-            <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+            <Alert text="Hello World"/>
         </div>
     );
 }
